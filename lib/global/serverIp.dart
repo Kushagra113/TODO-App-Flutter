d9bb41a542df223aadd01ef06ffb1ddc3f2b9eeb
kushagra113 <tibrewalkushagra@gmail.com>
2021-06-24 15:31:14 +0530

    Uncommented Server IP For Production

--- lib/global/serverIp.dart
+++ lib/global/serverIp.dart
@@ -1,10 +1,10 @@
 import 'package:todo_flutter_app/global/storage.dart' as globalStorage;
 
 // Hosted Server IP Link
-// final String severIp = "https://todo-app-tj.herokuapp.com";
+final String severIp = "https://todo-app-tj.herokuapp.com";
 
 // Emulator Server IP Link
-final String severIp = "http://10.0.2.2:8000";
+// final String severIp = "http://10.0.2.2:8000";
 
 // Physical Device IP Link
 // final String severIp = "http://192.168.43.243:8000";
